300395dd38c2643ace39fa07914b6c359a37292c
Maria Chira <geaguleamaria@yahoo.com>
2021-12-28 03:38:28 +0200

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -33,10 +33,11 @@ class App extends React.Component {
               ...snapShot.data()
             }
           });
-          console.log(this.state);
+         
         });
-      } 
+      } else {
         this.setState({ currentUser: userAuth });
+      }
       
     });
 
